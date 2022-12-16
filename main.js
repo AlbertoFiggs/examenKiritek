e672c23630ee2ad4babccdf94b9bf1bcf99c4cec
AlbertoFiggs <120751071+AlbertoFiggs@users.noreply.github.com>
2022-12-16 14:53:07 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
+import { auth } from "./app/firebase.js";
+import {} from './app/firebase.js';
+import './app/signupForm.js';
+import './app/signinForm.js';
+import './app/googleLogin.js';
+import './app/pokemonService.js';
+import './app/logout.js';
+import {loginCheck} from './app/loginCheck.js';
+
+
+onAuthStateChanged(auth, async (user) => {
+    loginCheck(user)
+
+    if(user){
+        loginCheck(user)
+    } else {
+        loginCheck(user)
+    }
+})
